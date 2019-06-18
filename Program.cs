867ef7b044102dc6dcce8c932f4230e77447f76f
gugu <gustavocorrea2032@gmail.com>
2019-06-18 11:19:16 -0300

    Verificar se o número é par ou impar; Bipar

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace ParImpar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+         int numero;
+
+           Console.WriteLine ("Digite um número: ");
+               numero = int.Parse(Console.ReadLine());
+
+         if (numero % 2 == 0) {
+           Console.WriteLine ("O número digitado é par!");
+                Console.Beep(3000,2000); 
+             }
+        
+         else {
+           Console.WriteLine ("O número digitado é impar!");
+                Console.Beep(500,2000);
+             }
+        }  
+
+        
     }
 }
